c9f12f8d50e9e6f2699d102110664479d7b258ff
Matthew Peterson <matt2694@edu.eal.dk>
2017-09-14 11:23:20 +0200

    done

--- UseService140917/MainWindow.xaml.cs
+++ UseService140917/MainWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace UseService140917
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Window_Loaded);
+        }
+
+        private PersonService.PersonWcfServiceClient _service = new PersonService.PersonWcfServiceClient();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PersonService.PersonDTO[] _personer = _service.GetAllePersoner();
+            foreach(PersonService.PersonDTO person in _personer)
+            {
+                list1.Items.Add("Address: " + person.Adresse + "\t" + "District: " + person.Distrikt + "\t" + "Last Name: " + person.Efternavn + "\t" + "First Name: " + person.Fornavn + "\t" + "Person Number: " + person.Personnr + "\t" + "Post Number: " + person.Postnr + "\t" + "Telephone Number: " + person.Tlfnr + "\t" + "Version: " + person.Version);
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
